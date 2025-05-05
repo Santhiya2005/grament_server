d0ff9825ac6e55e50ae30d4db9d742cf02a4aff3
Santhiya2005 <santhiyae587@gmail.com>
2025-05-05 11:13:07 +0530

    key

--- config/stripe.js
+++ config/stripe.js
@@ -1,5 +1,5 @@
 import stripe from 'stripe'
 
-const Stripe = stripe('sk_test_51QyVsmP8gkHsfJwVX5si08ILaO4wGU63EgdEPszRPIKqkrwr5ws9f7K9EnU7pTSgiLVk746uf6eNKO2FRhLruNNF001RanTYQf')
+const Stripe = stripe('')
 
 export default Stripe
